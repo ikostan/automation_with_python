f48cffe1a876201a34728332c1a17ff24df1eae8
ikostan <igorkostan@gmail.com>
2019-04-06 21:18:33 -0700

    Intro to methods

--- IntroToPython/exercise_1.py
+++ IntroToPython/exercise_1.py
@@ -5,3 +5,17 @@ var2 = 2
 # Create two variables, num1 and num2, which multiply together to give 16.
 num1, num2 = 2, 8
 print(num1 * num2)
+
+# Complete the method by making sure it returns 42. .
+
+
+def return_42():
+    # Complete method here
+    # pass  # 'pass' just means "do nothing". Make sure to delete this!
+    return 42
+
+
+# Create a method below, called my_method, that takes two arguments
+# and returns the result of its two arguments multiplied together.
+def my_method(arg_a, arg_b):
+    return arg_a * arg_b
--- IntroToPython/variables_methods.py
+++ IntroToPython/variables_methods.py
@@ -1,3 +1,4 @@
+# Lesson 1: variables
 a = 5
 b = 10
 my_variable = 56
@@ -6,9 +7,30 @@ my_10_variable = 10
 string_variable = 'hello'
 double_quotes_string = "Hello world"
 
-print(my_variable)
-print(string_variable)
-print(double_quotes_string)
-print("Hi there")
-print(123)
+#print(my_variable)
+#print(string_variable)
+#print(double_quotes_string)
+#print("Hi there")
+#print(123)
 
+# Lesson 2: methods
+
+
+def my_print_method(my_argument=''):
+    print("Hello")
+    print("World")
+    print(my_argument)
+
+
+#my_print_method()
+#my_print_method("hello, world")
+
+
+def my_multiply_method(number_one, number_two):
+    return number_one * number_two
+
+
+#result = my_multiply_method(5, 3)
+#print(result)
+#my_print_method(result)
+my_print_method(my_multiply_method(5, 3))
