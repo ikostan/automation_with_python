ca0de7a2aa3faccbaf6f7163eb864f742af4f9fd
ikostan <igorkostan@gmail.com>
2019-04-06 22:11:05 -0700

    Lists, sets, tuples

---
+++ IntroToPython/lists_tuples_sets.py
@@ -0,0 +1,33 @@
+my_variable = "hello"
+
+grade_one = 77
+grade_two = 80
+grade_three = 90
+
+# print((grade_one + grade_two + grade_three) / 3)
+
+grade_four = 95
+grade_five = 100
+
+grades_list = [grade_one, grade_two, grade_three, grade_four, grade_five]
+grades_tuple = (grade_four, grade_five, grade_one, grade_two, grade_three)
+grades_set = {77, 80, 90, 100, 100}
+
+print(sum(grades_list) / len(grades_list))
+grades_list.append(73)
+print(sum(grades_list) / len(grades_list))
+print(grades_set)
+print(sorted(grades_tuple))
+
+grades_tuple = grades_tuple + (66,)
+print(grades_tuple)
+print(sum(grades_tuple) / len(grades_tuple))
+
+for i in range(len(grades_set)):
+    print(grades_set.pop())
+
+print(grades_set)
+
+for i, b in enumerate(grades_tuple):
+    print("{0}: {1}".format(i, b))
+
