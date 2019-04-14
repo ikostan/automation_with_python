142c1e22c0ff61eeb663b46e0f740c6a4bf2906b
ikostan <igorkostan@gmail.com>
2019-04-14 15:06:43 -0700

    Exercise 4

--- IntroToPython/if_statements.py
+++ IntroToPython/if_statements.py
@@ -1,1 +1,10 @@
 print("IF statements")
+
+known_people = ["John", "Anna", "Mary"]
+person = input("Please enter person name: ")
+
+if person.capitalize() in known_people:
+    print(person + " is known person")
+else:
+    print(person + " is unknown person")
+
