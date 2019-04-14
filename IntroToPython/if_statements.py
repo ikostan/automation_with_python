51326c66b3498b4df5ab5666204ac6e6f52b1870
ikostan <igorkostan@gmail.com>
2019-04-14 15:29:43 -0700

    2 new methods added

--- IntroToPython/if_statements.py
+++ IntroToPython/if_statements.py
@@ -1,5 +1,6 @@
 print("IF statements")
 
+'''
 known_people = ["John", "Anna", "Mary"]
 person = input("Please enter person name: ")
 
@@ -7,4 +8,29 @@ if person.capitalize() in known_people:
     print(person + " is known person")
 else:
     print(person + " is unknown person")
+
+'''
+
+
+def who_do_you_know():
+    # Ask the user for a list of people they know
+    # Split the string into a list
+    user_input = input("Please enter names of people (separated by space) you know and press ENTER: ")
+    names = list(name.strip().capitalize() for name in user_input.split(" "))
+    #print(names)
+    return names
+
+
+def ask_user(names):
+    # Ask user for their name
+    # See if their name is in the list of people they know
+    # Print out that they know the person
+    user_name = input("Please enter your name and press ENTER: ")
+    if user_name.capitalize() in names:
+        print("You know the person")
+    else:
+        print("You do not know the person")
+
+
+ask_user(who_do_you_know())
 
