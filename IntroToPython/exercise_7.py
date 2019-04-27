dad55c9f27fc598b12c6e50be6274495f28670e0
ikostan <igorkostan@gmail.com>
2019-04-27 13:19:30 -0700

    exercise #7

---
+++ IntroToPython/exercise_7.py
@@ -0,0 +1,27 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        self.items.append({
+            'name': name,
+            'price': price
+        })
+
+    def stock_price(self):
+        total = 0
+        for item in self.items:
+            total += item['price']
+        return total
+
+    @classmethod
+    def franchise(cls, store):
+        # Return another store, with the same name as the argument's name, plus " - franchise"
+        return Store(store.name + " - franchise")
+
+    @staticmethod
+    def store_details(store):
+        # Return a string representing the argument
+        # It should be in the format 'NAME, total stock price: TOTAL'
+        return "{0}, total stock price: {1}".format(store.name, store.stock_price())
