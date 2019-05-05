8f70f3c41965b4585b0af1246b40de900512e858
ikostan <igorkostan@gmail.com>
2019-05-05 11:32:56 -0700

    Minor comments

--- blog/post.py
+++ blog/post.py
@@ -1,8 +1,12 @@
 class Post:
     def __init__(self, title: str, content: str):
+        # Post Tile (capitalized)
         self.title = title.capitalize()
+
+        # Post Content
         self.content = content
 
+    # JSON
     def json(self):
         return {
             'title': self.title,
