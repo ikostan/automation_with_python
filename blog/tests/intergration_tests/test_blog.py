367bf28115d54bc23af9980a46cbdc2de2370dcb
ikostan <igorkostan@gmail.com>
2019-05-11 16:33:39 -0700

    Integration test for blog.json

--- blog/tests/intergration_tests/test_blog.py
+++ blog/tests/intergration_tests/test_blog.py
@@ -2,7 +2,6 @@ import unittest
 from blog.blog import Blog
 
 
-
 class MyTestCase(unittest.TestCase):
     def test_create_post_in_blog(self):
         title = 'Test'
@@ -13,6 +12,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(1, len(new_blog.posts))
         self.assertEqual('Test Post', new_blog.posts[0].title)
         self.assertEqual('Test content', new_blog.posts[0].content)
+
+    def test_json(self):
+        title = 'Test'
+        author = 'first last'
+        new_blog = Blog(title, author)
+        new_blog.create_post('Test post', 'Test content')
+        expected = {'title': title,
+                    'author': 'First Last',
+                    'posts': [{'title': 'Test Post', 'content': 'Test content'}]}
+        # Tests
+        self.assertDictEqual(expected, new_blog.json())
 
 
 if __name__ == '__main__':
