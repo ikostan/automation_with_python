0a0fabef8db38df70c778d0ea74d40515acc5536
ikostan <igorkostan@gmail.com>
2019-05-20 18:04:29 -0700

    TODO item for test_suite file

--- blog_console_app/tests/test_suite.py
+++ blog_console_app/tests/test_suite.py
@@ -1,4 +1,7 @@
 import unittest
+
+# TODO: refactor the file in order to make it as a test and not just python file
+
 loader = unittest.TestLoader()
 start_dir = 'C:/Users/superadmin/Desktop/Python/automation_with_python/blog_console_app/tests'
 suite = loader.discover(start_dir)
