637d458cfb0cbabb6d5d676ea9dd1c446f19b7a9
ikostan <igorkostan@gmail.com>
2019-05-20 19:51:47 -0700

    First system test for flask app
    test response code == 200

--- flask_app/test/system/test_home.py
+++ flask_app/test/system/test_home.py
@@ -1,10 +1,19 @@
 import unittest
+import os
 from flask_app.app import app
 
 
 class HomeTestCase(unittest.TestCase):
+
+    print("Running unit tests from: " + os.path.dirname(__file__) + '\\' + os.path.basename(__file__) + "\n")
+
     def test_home(self):
-        self.assertEqual(True, False)
+        # make a request for the app
+        with app.test_client() as client:
+            #  response variable
+            resp = client.get('/')
+            # test response status code, should be 200
+            self.assertEqual(resp.status_code, 200)
 
 
 if __name__ == '__main__':
