f17213a8cf47dce7bd7c8c76fc740fc35f914a27
ikostan <igorkostan@gmail.com>
2019-05-24 17:46:51 -0700

    Base Test implementation

---
+++ flask_app/test/system/base_test.py
@@ -0,0 +1,12 @@
+import unittest
+from flask_app.app import app
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+
+if __name__ == '__main__':
+    unittest.main()
--- flask_app/test/system/test_home.py
+++ flask_app/test/system/test_home.py
@@ -1,23 +1,25 @@
-import unittest
+from flask_app.test.system.base_test import BaseTestCase
 import os
 import json
-from flask_app.app import app
 
 
-class HomeTestCase(unittest.TestCase):
+class HomeTestCase(BaseTestCase):
 
     print("Running unit tests from: " + os.path.dirname(__file__) + '\\' + os.path.basename(__file__) + "\n")
 
-    def test_home(self):
+    def test_home_response_code(self):
         # make a request for the app with a test client
-        with app.test_client() as client:
+        with self.app as client:
             #  response variable
             resp = client.get('/')
             # test response status code, should be 200
             self.assertEqual(resp.status_code, 200)
+
+    def test_home_json_data(self):
+        # make a request for the app with a test client
+        with self.app as client:
+            #  response variable
+            resp = client.get('/')
             # verify json data by loading string and converting it into json/dictionary
             self.assertEqual(json.loads(resp.get_data()), {'message': 'Hello, world!'})
 
-
-if __name__ == '__main__':
-    unittest.main()
