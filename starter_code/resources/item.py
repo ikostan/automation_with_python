a22607315e5ed17ae1ccbad4f3868499417209e3
ikostan <igorkostan@gmail.com>
2019-05-27 19:26:48 -0700

    item.py fixes

--- starter_code/resources/item.py
+++ starter_code/resources/item.py
@@ -1,5 +1,4 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
 from starter_code.models.item import ItemModel
 
 
@@ -10,7 +9,6 @@ class Item(Resource):
                         required=True,
                         help="This field cannot be left blank!")
 
-    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
