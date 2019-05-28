612817188222a43f4fb7eaed9d694ef5c4fca899
ikostan <igorkostan@gmail.com>
2019-05-27 19:59:25 -0700

    ItemModel minor comments

--- starter_code/tests/unit/models/test_item.py
+++ starter_code/tests/unit/models/test_item.py
@@ -5,16 +5,19 @@ from starter_code.models.item import ItemModel
 
 class ItemModelTestCase(unittest.TestCase):
 
+    #  printing out the running unit test file location/name
     print("Running unit tests from: " +
           os.path.dirname(__file__) +
           '\\' + os.path.basename(__file__) +
           "\n")
 
+    #  prerequisites
     def setUp(self):
         self.name = 'product'
         self.price = 99.99
         self.itemModel = ItemModel(self.name, self.price)
 
+    #  testing init method
     def test_init(self):
         self.assertEqual(self.name,
                          self.itemModel.name,
@@ -25,6 +28,7 @@ class ItemModelTestCase(unittest.TestCase):
                          "ItemModel 'price' arg value {0} does not match the inserted data {1}".
                          format(self.itemModel.price, self.price))
 
+    #  testing JSON method
     def test_json(self):
         pass
 
