ca31d73841a4c339f6b8aff818344354517fdfeb
ikostan <igorkostan@gmail.com>
2019-05-28 22:02:33 -0700

    minor changes

--- starter_code_section_6/models/store.py
+++ starter_code_section_6/models/store.py
@@ -1,4 +1,4 @@
-from db import db
+from starter_code_section_6.db import db
 
 
 class StoreModel(db.Model):
--- starter_code_section_6/resources/store.py
+++ starter_code_section_6/resources/store.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from models.store import StoreModel
+from starter_code_section_6.models.store import StoreModel
 
 
 class Store(Resource):
