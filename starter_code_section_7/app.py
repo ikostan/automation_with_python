b186f845aec781acab694c4e1dab9e452dc3d81f
ikostan <igorkostan@gmail.com>
2019-05-31 16:57:19 -0700

    authorization func

--- starter_code_section_7/app.py
+++ starter_code_section_7/app.py
@@ -1,11 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
+from flask_jwt import JWT, JWTError
 
 from starter_code_section_7.resources.item import Item, ItemList
 from starter_code_section_7.resources.store import Store, StoreList
 from starter_code_section_7.resources.user import UserRegistration
+from starter_code_section_7.security import authenticate, identity
+
 
 app = Flask(__name__)
 
@@ -13,14 +16,21 @@ app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'secretkey'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegistration, '/register')
+
+
+@app.errorhandler(JWTError)
+def auth_error_handler(err):
+    return jsonify({'message': 'Could not authorize. Invalid Authorization header!'}), 401
 
 
 if __name__ == '__main__':
