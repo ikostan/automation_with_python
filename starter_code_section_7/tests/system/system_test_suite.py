a956e48b370749db5da1cc87ce784849911ea6a3
ikostan <igorkostan@gmail.com>
2019-05-31 19:43:33 -0700

    Test suites for unit, integration and system tests

--- starter_code_section_7/tests/system/system_test_suite.py
+++ starter_code_section_7/tests/system/system_test_suite.py
@@ -3,9 +3,10 @@ import unittest
 # TODO: refactor the file in order to make it as a test and not just python file
 
 loader = unittest.TestLoader()
-start_dir = 'C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests'
-suite = loader.discover(start_dir)
 
+system = 'C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/system'
+
+suite_system = loader.discover(system)
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(suite)
+runner.run(suite_system)
 
