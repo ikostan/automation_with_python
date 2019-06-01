2e31def4a00655e5f6ab158ab5f21e064a7fd04f
ikostan <igorkostan@gmail.com>
2019-05-31 20:34:22 -0700

    A new method to run all test suites

--- starter_code_section_7/tests/test_suite.py
+++ starter_code_section_7/tests/test_suite.py
@@ -1,26 +1,17 @@
-#!/bin/bash
-def execfile(filepath, globals=None, locals=None):
-    if globals is None:
-        globals = {}
-    globals.update({
-        "__file__": filepath,
-        "__name__": "__main__",
-    })
-    with open(filepath, 'rb') as file:
-        exec(compile(file.read(), filepath, 'exec'), globals, locals)
+import runpy
+import time
 
+
+unit = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/unit/unit_test_suite.py"
+integration = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/integration/integration_test_suite.py"
+system = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/system/system_test_suite.py"
+
+files = {'UNIT': unit, 'INTEGRATION': integration, 'SYSTEM': system}
 
 # execute the file
-print('\nRunning UNIT test suite:')
-unit = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/unit/unit_test_suite.py"
-execfile(unit)
-
-print('\nRunning INTEGRATION test suite:')
-integration = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/integration/integration_test_suite.py"
-execfile(integration)
-
-
-print('\nRunning SYSTEM test suite:')
-system = "C:/Users/superadmin/Desktop/Python/automation_with_python/starter_code_section_7/tests/system/system_test_suite.py"
-execfile(system)
+for key in files.keys():
+    print('\nRunning {0} test suite:'.format(key))
+    run = runpy.run_path(files[key])
+    run.clear()
+    time.sleep(1)
 
