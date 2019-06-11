cb2e84d6c875857f41316f10d1636b895b0bb608
ikostan <igorkostan@gmail.com>
2019-06-11 15:43:52 -0700

    Create a setUp method for Item system test

--- starter_code_section_7/tests/system/models/test_system_item.py
+++ starter_code_section_7/tests/system/models/test_system_item.py
@@ -7,19 +7,7 @@ import json
 
 class ItemSystemTest(BaseTest):
 
-    def test_get_item_no_auth(self):
-        with self.app() as client:
-            with self.app_context():
-                item_name = 'test item'
-                resp = client.get('item/{}'.format(item_name))
-
-                # Authorisation error code
-                self.assertEqual(401, resp.status_code)
-                # Authorisation error message
-                self.assertDictEqual({'message': 'Could not authorize. Invalid Authorization header!'},
-                                     json.loads(resp.data))
-
-    def test_get_item_not_found(self):
+    def setUp(self):
         with self.app() as client:
             with self.app_context():
                 # create a user
@@ -36,10 +24,26 @@ class ItemSystemTest(BaseTest):
                 # authorisation token
                 jwt_token = json.loads(auth_request.data)["access_token"]
 
-                header = {'Authorization': 'JWT ' + jwt_token}
+                self.header = {'Authorization': 'JWT ' + jwt_token}
+
+    def test_get_item_no_auth(self):
+        with self.app() as client:
+            with self.app_context():
+                item_name = 'test item'
+                resp = client.get('item/{}'.format(item_name))
+
+                # Authorisation error code
+                self.assertEqual(401, resp.status_code)
+                # Authorisation error message
+                self.assertDictEqual({'message': 'Could not authorize. Invalid Authorization header!'},
+                                     json.loads(resp.data))
+
+    def test_get_item_not_found(self):
+        with self.app() as client:
+            with self.app_context():
 
                 item_name = 'test item'
-                resp = client.get('item/{}'.format(item_name), headers=header)
+                resp = client.get('item/{}'.format(item_name), headers=self.header)
 
                 # Item not found error code
                 self.assertEqual(404, resp.status_code)
@@ -48,10 +52,41 @@ class ItemSystemTest(BaseTest):
                                      json.loads(resp.data))
 
     def test_get_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+
+                # create an item
+                item_name = 'ChromeBook'
+                item_price = 199.99
+                item_id = 1
+                ItemModel(item_name, item_price, item_id).save_to_db()
+
+                resp = client.get('item/{}'.format(item_name), headers=self.header)
+
+                # Item response code
+                self.assertEqual(200, resp.status_code)
+                # Item response message
+                self.assertDictEqual({'name': 'ChromeBook', 'price': 199.99},
+                                     json.loads(resp.data))
 
     def test_delete_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+
+                # create an item
+                item_name = 'ChromeBook'
+                item_price = 199.99
+                item_id = 1
+                ItemModel(item_name, item_price, item_id).save_to_db()
+                self.assertIsNotNone(ItemModel.find_by_name(item_name))
+
+                resp = client.get('item/{}'.format(item_name), headers=self.header)
+
+                # response code
+                self.assertEqual(200, resp.status_code)
+                # response message
+                self.assertDictEqual({'message': 'Item deleted'},
+                                     json.loads(resp.data))
 
     def test_create_item(self):
         pass
