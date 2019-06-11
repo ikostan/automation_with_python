6a8d78f44b00f678c7d3bae893047bbc61851990
ikostan <igorkostan@gmail.com>
2019-06-10 21:15:57 -0700

    Start system test for Item

--- starter_code_section_7/tests/system/models/test_system_item.py
+++ starter_code_section_7/tests/system/models/test_system_item.py
@@ -1,9 +1,20 @@
 from starter_code_section_7.tests.base_test import BaseTest
+from starter_code_section_7.models.store import StoreModel
+from starter_code_section_7.models.user import UserModel
 from starter_code_section_7.models.item import ItemModel
+import json
 
 
 class ItemSystemTest(BaseTest):
     def test_create_item(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                store_name = "Electronics"
+                store_id = 1
+                StoreModel(store_name).save_to_db()
+
+                item_name = "Chrome Book"
+                item_price = 199.99
+
 
 
