765946558e020aed65fa05a837901bb0e9e477b4
ikostan <igorkostan@gmail.com>
2019-06-27 13:29:45 -0700

    Update stop_app.py

--- video_code_section_10/stop_app.py
+++ video_code_section_10/stop_app.py
@@ -1,6 +1,7 @@
 import http.client
 import urllib.parse
-params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+
+params = urllib.parse.urlencode({})
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 conn = http.client.HTTPConnection("127.0.0.1:5000")
 conn.request("POST", "/shutdown", params, headers)
