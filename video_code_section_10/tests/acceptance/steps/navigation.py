0a7ce665a929ec73b98c8108ef4046e9dc11e51c
ikostan <igorkostan@gmail.com>
2019-06-26 19:41:58 -0700

    Section 10
    Finishing first test

--- video_code_section_10/tests/acceptance/steps/navigation.py
+++ video_code_section_10/tests/acceptance/steps/navigation.py
@@ -6,7 +6,15 @@ use_step_matcher('re')
 
 @given('I am on the home page')
 def step_impl(context):
+
     path = 'C:/Users/superadmin/Desktop/Python/automation_with_python/' \
            'video_code_section_10/webdriver/win32/74.0.3729.6/chromedriver.exe'
-    browser = webdriver.Chrome(path)
-    browser.get('http://127.0.0.1:5000')
+    context.browser = webdriver.Chrome(path)
+    context.browser.get('http://127.0.0.1:5000')
+
+
+@then('I am on the blog page')
+def step_impl(context):
+
+    expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.browser.current_url == expected_url
