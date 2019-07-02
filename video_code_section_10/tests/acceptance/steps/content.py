7d53da223f372bee258f0cc207e5c5a912f9943f
ikostan <igorkostan@gmail.com>
2019-07-02 12:13:19 -0700

    Update content.py

--- video_code_section_10/tests/acceptance/steps/content.py
+++ video_code_section_10/tests/acceptance/steps/content.py
@@ -7,16 +7,14 @@ use_step_matcher('re')
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    # title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
-    # assert title_tag.is_displayed()
+
     page = BasePage(context.driver)
     assert page.title.is_displayed()
 
 
 @step('The title has content "(.*)"')
 def step_impl(context, content):
-    # title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
-    # assert title_tag.text == content
+
     page = BasePage(context.driver)
     assert page.title.text == content
 
